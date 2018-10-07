a6e0fa18d46828717b04724edeab8325fd634073
Alvin <alvinramoutar@gmail.com>
2018-10-07 13:30:42 -0400

    Formatting

--- kf2server-telegrambot/Program.cs
+++ kf2server-telegrambot/Program.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace kf2server_telegrambot
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace kf2server_telegrambot {
+    class Program {
+        static void Main(string[] args) {
         }
     }
 }
